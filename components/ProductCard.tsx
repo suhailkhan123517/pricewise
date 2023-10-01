23f873c865636a9b770e377567d33c1cb5adf1c4
suhailkhan123517 <skhan282290@gmail.com>
2023-10-01 18:33:28 +0530

    sunday

---
+++ components/ProductCard.tsx
@@ -0,0 +1,12 @@
+import { Product } from "@/types";
+import React from "react";
+
+interface Props {
+  product: Product;
+}
+
+const ProductCard = ({ product }: Props) => {
+  return <div>ProductCard</div>;
+};
+
+export default ProductCard;
