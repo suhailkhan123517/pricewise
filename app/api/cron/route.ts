6eedf49f50854ed4943d02be54e7917a0c4defe2
Mohammad suhail <skhan282290@gmail.com>
2023-10-03 08:51:40 +0530

    fix bug

--- app/api/cron/route.ts
+++ app/api/cron/route.ts
@@ -10,7 +10,7 @@ import {
 } from "@/lip/utils";
 import { NextResponse } from "next/server";
 
-export const maxDuration = 300; // This function can run for a maximum of 300 seconds
+export const maxDuration = 10; // This function can run for a maximum of 300 seconds
 export const dynamic = "force-dynamic";
 export const revalidate = 0;
 
