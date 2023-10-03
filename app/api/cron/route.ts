0bd9aa9197bbb0282e660ae1892ff7bd9b2d3fd3
Mohammad suhail <skhan282290@gmail.com>
2023-10-03 08:45:35 +0530

    2nd commit

--- app/api/cron/route.ts
+++ app/api/cron/route.ts
@@ -10,6 +10,10 @@ import {
 } from "@/lip/utils";
 import { NextResponse } from "next/server";
 
+export const maxDuration = 300; // This function can run for a maximum of 300 seconds
+export const dynamic = "force-dynamic";
+export const revalidate = 0;
+
 export async function GET() {
   try {
     connectToDB();
